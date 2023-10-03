fb728de86e078c93beb24a1639fa7de7a11a2b11
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-03 19:42:40 +0500

    uzunligi toq bo'lsa chiqarish

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,24 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    if len(s1)%2==1:
+        if len(s2)%2==1:
+            if len(s3)%2==1:
+                return s1,s2,s3
+            else:
+                return s1,s2
+        else:
+            return s1
+    else:
+        if len(s2)%2==1:
+            if len(s3)%2==1:
+                return s1,s2
+            else:
+                return s2
+        else:
+            if len(s3)%2==1:
+                return s3
+            else :
+                return "[]"
+print(main("code", "python", "coder"))
+print(main("codeschool.uz","example","python"))
